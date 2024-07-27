9100485384a5b4c40cec87cb13362194b4502d50
AlexandreSato <sato_alexandre@hotmail.com>
2024-07-27 09:16:10 -0300

    test checksum

--- can/tests/test_checksums.py
+++ can/tests/test_checksums.py
@@ -4,32 +4,31 @@ from opendbc.can.tests.test_packer_parser import can_list_to_can_capnp
 
 
 class TestCanChecksums:
+  print('baba')
 
-  def test_honda_checksum(self):
-    """Test checksums for Honda standard and extended CAN ids"""
-    dbc_file = "honda_accord_2018_can_generated"
-    msgs = [("LKAS_HUD", 0), ("LKAS_HUD_A", 0)]
-    parser = CANParser(dbc_file, msgs, 0)
+  def test_chrysler_checksum(speed):
+    dbc_file = "chrysler_pacifica_2017_hybrid_generated"
+    msg = [("WHEEL_SPEEDS", 0)]
+    parser = CANParser(dbc_file, msg, 0)
     packer = CANPacker(dbc_file)
 
     values = {
-      'SET_ME_X41': 0x41,
-      'STEERING_REQUIRED': 1,
-      'SOLID_LANES': 1,
-      'BEEP': 0,
+      'WHEEL_SPEED_LEFT': speed,
+      'WHEEL_SPEED_RIGHT': speed,
     }
 
-    # known correct checksums according to the above values
-    checksum_std = [11, 10, 9, 8]
-    checksum_ext = [4, 3, 2, 1]
+    # observed checksums of above values in cabana
+    counter =          [ 0,   1,   2,  3,   4,   5,   6,  7,   8,  9, 10, 11, 12, 13,  14,  15]
+    correct_checksum = [55, 129, 166, 47, 177, 172, 193, 20, 177, 25, 52, 28, 35, 86, 113, 108]
 
-    for std, ext in zip(checksum_std, checksum_ext):
-      msgs = [
-        packer.make_can_msg("LKAS_HUD", 0, values),
-        packer.make_can_msg("LKAS_HUD_A", 0, values),
-      ]
+    for id, counter_checksum in zip(counter, correct_checksum):
+      msgs = [packer.make_can_msg('WHEEL_SPEEDS', 0, values),]
       can_strings = [can_list_to_can_capnp(msgs), ]
       parser.update_strings(can_strings)
+      # print(f'oh babi: {id:02d}')
+      # assert parser.vl['WHEEL_SPEEDS']['CHECKSUM'] == counter_checksum
+      if parser.vl['WHEEL_SPEEDS']['CHECKSUM'] == counter_checksum:
+        print(f'match in checksum: {counter_checksum:03d} and speed: {speed:06d} and counter: {id:02d}')
 
-      assert parser.vl['LKAS_HUD']['CHECKSUM'] == std
-      assert parser.vl['LKAS_HUD_A']['CHECKSUM'] == ext
+  for speed in range(6218501, 6226698):
+    test_chrysler_checksum(speed)
