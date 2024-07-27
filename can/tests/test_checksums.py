9005e0e9b8810e161b2a2f50c9dd915a465d33cf
AlexandreSato <sato_alexandre@hotmail.com>
2024-07-27 16:48:22 -0300

    w00t checksum worked

--- can/tests/test_checksums.py
+++ can/tests/test_checksums.py
@@ -1,34 +1,38 @@
+#!/usr/bin/env python3
+import unittest
+
 from opendbc.can.parser import CANParser
 from opendbc.can.packer import CANPacker
 from opendbc.can.tests.test_packer_parser import can_list_to_can_capnp
 
 
-class TestCanChecksums:
+class TestCanChecksums(unittest.TestCase):
   print('baba')
 
-  def test_chrysler_checksum(speed):
-    dbc_file = "chrysler_pacifica_2017_hybrid_generated"
+  def test_chrysler_checksum(self):
+    dbc_file = "chrysler_jeep_commander_generated"
     msg = [("WHEEL_SPEEDS", 0)]
     parser = CANParser(dbc_file, msg, 0)
     packer = CANPacker(dbc_file)
 
     values = {
-      'WHEEL_SPEED_LEFT': speed,
-      'WHEEL_SPEED_RIGHT': speed,
+      'WHEEL_SPEED_FL':  989,
+      'WHEEL_SPEED_FR': 1002,
+      'WHEEL_SPEED_RL':  987,
+      'WHEEL_SPEED_RR':  994,
     }
 
     # observed checksums of above values in cabana
-    counter =          [ 0,   1,   2,  3,   4,   5,   6,  7,   8,  9, 10, 11, 12, 13,  14,  15]
-    correct_checksum = [55, 129, 166, 47, 177, 172, 193, 20, 177, 25, 52, 28, 35, 86, 113, 108]
+    counter =          [  0,   1,   2,   3,   4]
+    correct_checksum = [213, 200, 239, 242, 161]
 
     for id, counter_checksum in zip(counter, correct_checksum):
+      # values.append('COUNTER': id,)
       msgs = [packer.make_can_msg('WHEEL_SPEEDS', 0, values),]
       can_strings = [can_list_to_can_capnp(msgs), ]
       parser.update_strings(can_strings)
-      # print(f'oh babi: {id:02d}')
-      # assert parser.vl['WHEEL_SPEEDS']['CHECKSUM'] == counter_checksum
-      if parser.vl['WHEEL_SPEEDS']['CHECKSUM'] == counter_checksum:
-        print(f'match in checksum: {counter_checksum:03d} and speed: {speed:06d} and counter: {id:02d}')
+      print(f'oh babi: {id:02d} | checksum from parser is: {int(parser.vl['WHEEL_SPEEDS']['CHECKSUM']):03d}')
+      self.assertEqual(parser.vl['WHEEL_SPEEDS']['CHECKSUM'], counter_checksum)
 
-  for speed in range(6218501, 6226698):
-    test_chrysler_checksum(speed)
+if __name__ == "__main__":
+  unittest.main()
