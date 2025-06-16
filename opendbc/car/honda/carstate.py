34e717e0b609e03683917940802644d11c0996e3
AlexandreSato <sato_alexandre@hotmail.com>
2025-06-16 19:27:08 -0300

    add honda city handbrake signal

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -52,9 +52,9 @@ def get_can_messages(CP, gearbox_msg):
   if CP.flags & HondaFlags.BOSCH_ALT_BRAKE:
     messages.append(("BRAKE_MODULE", 50))
 
-  # TODO: Fix this properly for Honda City, manual hand brake only
-  #if CP.carFingerprint in (HONDA_BOSCH | {CAR.HONDA_CIVIC, CAR.HONDA_ODYSSEY, CAR.HONDA_ODYSSEY_CHN}):
-    #messages.append(("EPB_STATUS", 50))
+  # TODO: Fix this properly for Honda City, manual hand brake or electronical detect dynamically
+  if CP.carFingerprint in (HONDA_BOSCH | {CAR.HONDA_CIVIC, CAR.HONDA_ODYSSEY, CAR.HONDA_ODYSSEY_CHN}):
+    messages.append(("EPB_STATUS", 50))
 
   if CP.carFingerprint in HONDA_BOSCH:
     # these messages are on camera bus on radarless cars
@@ -188,9 +188,11 @@ class CarState(CarStateBase):
     ret.brakeHoldActive = cp.vl["VSA_STATUS"]["BRAKE_HOLD_ACTIVE"] == 1
 
     # TODO: set for all cars
-    # TODO: Fix this properly for Honda City, manual hand brake only
-    #if self.CP.carFingerprint in (HONDA_BOSCH | {CAR.HONDA_CIVIC, CAR.HONDA_ODYSSEY, CAR.HONDA_ODYSSEY_CHN}):
-      #ret.parkingBrake = cp.vl["EPB_STATUS"]["EPB_STATE"] != 0
+    if self.CP.carFingerprint in (HONDA_BOSCH | {CAR.HONDA_CIVIC, CAR.HONDA_ODYSSEY, CAR.HONDA_ODYSSEY_CHN}):
+      ret.parkingBrake = cp.vl["EPB_STATUS"]["EPB_STATE"] != 0
+    # TODO: Fix this properly for Honda City, manual hand brake or Electronical detect dynamically
+    elif self.CP.carFingerprint == CAR.HONDA_CITY:
+      ret.parkingBrake = cp.vl["SCM_FEEDBACK"]["HANDBRAKE_ON"] != 0
 
     if self.CP.transmissionType == TransmissionType.manual:
       ret.clutchPressed = cp.vl["GEARBOX_ALT_2"]["GEAR_MT"] == 0
